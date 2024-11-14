63d05cfc1e806277e6bfa921fae874b6bfc937c0
Rahul-Das-Sarma <rahuldas35748@gmail.com>
2024-11-14 22:31:08 +0530

    feat: add tab component

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ export default function Home() {
       <Tabs>
         <Tab title="Tab 1">Content 1</Tab>
         <Tab title="Tab 2">Content 2</Tab>
+        <Tab title="Tab 3">Content 3</Tab>
+        <Tab title="Tab 4">Content 4</Tab>
       </Tabs>
     </div>
   );
