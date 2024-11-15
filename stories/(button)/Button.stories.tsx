aa7faf21b5b2d333ba0f8946336a84f178d5984c
kaushikdut <duttakoushik779@gmail.com>
2024-11-15 20:58:57 +0530

    feat: add a loading state in btn and change the btn headings from label to children

--- stories/(button)/Button.stories.tsx
+++ stories/(button)/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StoryFn, Meta } from "@storybook/react";
-import { Button, ToggleButton } from "../../components";
+import { Button } from "../../components";
 import { action } from "@storybook/addon-actions";
 
 export default {
@@ -19,7 +19,6 @@ export default {
         "outline",
         "gradient",
         "glass",
-        "neumorphic",
         "glowOnHover",
         "icon",
       ],
@@ -31,65 +30,74 @@ const Button1: StoryFn<typeof Button> = (args) => <Button {...args} />;
 
 export const Primary = Button1.bind({});
 Primary.args = {
-  label: "Primary Button",
+  children: "Primary Button",
   variant: "primary",
   size: "medium",
+  isLoading: false,
   onClick: action("Primary button clicked"),
 };
 
 export const Secondary = Button1.bind({});
 Secondary.args = {
-  label: "Secondary Button",
+  children: "Secondary Button",
   variant: "secondary",
   size: "medium",
+  isLoading: false,
   onClick: action("Secondary button clicked"),
 };
 
 // New button variant stories
 export const Outline = Button1.bind({});
 Outline.args = {
-  label: "Outline Button",
+  children: "Outline Button",
   variant: "outline",
   size: "medium",
+  isLoading: false,
   onClick: action("Outline button clicked"),
 };
 
 export const Gradient = Button1.bind({});
 Gradient.args = {
-  label: "Gradient Button",
+  children: "Gradient Button",
   variant: "gradient",
   size: "medium",
+  isLoading: false,
   onClick: action("Gradient button clicked"),
 };
 
 export const Glass = Button1.bind({});
 Glass.args = {
-  label: "Glass Button",
+  children: "Glass Button",
   variant: "glass",
   size: "medium",
+  isLoading: false,
   onClick: action("Glass button clicked"),
 };
 
-export const Neumorphic = Button1.bind({});
-Neumorphic.args = {
-  label: "Neumorphic Button",
-  variant: "neumorphic",
-  size: "medium",
-  onClick: action("Neumorphic button clicked"),
-};
 export const DisabledPrimary = Button1.bind({});
 DisabledPrimary.args = {
-  label: "Disabled Primary Button",
+  children: "Disabled Primary Button",
   variant: "primary",
   size: "medium",
   disabled: true,
+  isLoading: false,
   onClick: action("Primary button clicked"),
+};
+
+export const Loading = Button1.bind({});
+Loading.args = {
+  children: "Loading Button",
+  variant: "primary",
+  size: "medium",
+  isLoading: true,
+  onClick: action("Loading button clicked"),
 };
 
 export const glowOnHover = Button1.bind({});
 glowOnHover.args = {
-  label: "Glow-on-hover Button",
+  children: "Glow-on-hover Button",
   variant: "glowOnHover",
   size: "medium",
+  isLoading: false,
   onClick: action("Glow-on-hover button clicked"),
 };
